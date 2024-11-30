b22a793819ebee53a081f4660015c08a10ad417f
Max Lerman <mjl82601@gmail.com>
2024-11-30 21:58:14 +0000

    started products page

--- mobile-app/src/app/(shop)/index.tsx
+++ mobile-app/src/app/(shop)/index.tsx
@@ -1,13 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { PRODUCTS } from "../../core/products";
+import { ProductListItem } from "../../components/product-list-item";
 
 const Home = () => {
   return (
     <View style={""}>
-      <Text>Welcome to the Gadget Shop!</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flastListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flastListColumn: {
+    justifyContent: "space-between",
+  },
+});
