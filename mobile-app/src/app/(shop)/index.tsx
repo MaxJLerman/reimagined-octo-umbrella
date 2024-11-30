0abedac016249eff837b674c1a7e3903b3a0f6fd
Max Lerman <mjl82601@gmail.com>
2024-11-30 21:34:56 +0000

    feat: established tabs & pages

---
+++ mobile-app/src/app/(shop)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={""}>
+      <Text>Welcome to the Gadget Shop!</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
